24e95ae22b3f4187d2d34e074db75b6950461307
qiweb2018 <zhangqi@dbgo.cn>
2018-08-16 15:11:28 +0800

    config-server

--- config-client/src/main/java/com/qiweb/ConfigClientApplication.java
+++ config-client/src/main/java/com/qiweb/ConfigClientApplication.java
@@ -3,10 +3,12 @@ package com.qiweb;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableDiscoveryClient  // 服务注册与发现
 @RestController
 public class ConfigClientApplication {
 
--- config-server/src/main/java/com/qiweb/ConfigServerApplication.java
+++ config-server/src/main/java/com/qiweb/ConfigServerApplication.java
@@ -2,9 +2,11 @@ package com.qiweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableDiscoveryClient  // 服务注册与发现
 @EnableConfigServer
 public class ConfigServerApplication {
 
